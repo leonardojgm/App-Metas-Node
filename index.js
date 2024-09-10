a8b5c4d0c45369e18227e884bbcb074e117ac24d
leonardojgm <cookie.maia@gmail.com>
2024-09-09 21:31:27 -0300

    Fundamentos 003

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
-// olá, mundo!
-const mensagem = "olá, mundo!";
 
-{
-    const mensagem = "olá eu"
-
-    console.log(mensagem);
-}
-
-console.log(mensagem);
-
-// arrays, objetos
 let meta = {
     value: 'ler um livro por mês',
-    address: 2,
-    checked: true,
-    log: (info) => {
-        console.log(info);
-    }
+    checked: true
 }
 
-meta.value = "não é mais ler um livro";
-meta.log(meta.value);
+let metas = [
+    meta,
+    {
+        value: 'caminhar 20 minutos todos os dias',
+        checked: false
+    }
+]
 
-// functions // arrow function
-const criarMeta = () => {}
-
-//function criarMeta () {}
+console.log(metas[0].value)
